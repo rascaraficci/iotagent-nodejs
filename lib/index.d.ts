673df052b5510df1c65a2d3e9a5bb6bad5fc4808
Raul Negreiros <rauln@cpqd.com.br>
2018-10-07 09:31:27 -0300

    Modifies the init to return a promise
    - Modifies the init to return a promise
    - Removes unused parameter on constructor
    - Updates typescript description file to sync the methods

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,18 +1,13 @@
 declare module 'dojot-iotagent' {
   export class IoTAgent {
-    constructor (config?: any);
+    constructor ();
 
-    init(): void;
+    init(): Promise<any>;;
 
     getDevice(deviceid: string, tenant: string): Promise<any>;
-    listDevices(tenant: string, query?: any): Promise<any>;
-    listTenants(): Promise<any>;
-
-    on(event:string, callback:(data?:any) => void): void;
-
+    
     updateAttrs(deviceid:string, tenant:string, attrs:any, metadata?:any): void;
 
-    on(event:string, callback:() => void): void;
     setOnline(deviceid: string, tenant:string, expires:Date): void;
     setOffline(deviceid: string, tenant:string): void;
   }
