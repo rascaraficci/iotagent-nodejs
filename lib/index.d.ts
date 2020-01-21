c74e6f4dfaa98f4d2ff2e9d97e9e98f3f5d275ee
Mariane Previde <mprevide@cpqd.com.br>
2020-01-21 15:57:27 -0300

    add new version dojot/modules

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,18 +1,18 @@
 declare module '@dojot/iotagent-nodejs' {
   export class IoTAgent {
-    constructor ();
+    constructor();
     init(): Promise<any>;;
     getDevice(deviceid: string, tenant: string): Promise<any>;
     checkCompleteMetaFields(deviceid: string, tenant: string, metadata: any): any;
     updateAttrs(deviceid: string, tenant: string, attrs: any, metadata: any, key?: string | null): void;
-    setOnline(deviceid: string, tenant:string, expires:Date): void;
-    setOffline(deviceid: string, tenant:string): void;
+    setOnline(deviceid: string, tenant: string, expires: Date): void;
+    setOffline(deviceid: string, tenant: string): void;
     on(subject: string, event: string, callback: (tenant: string, data: any, extraInfo?: any) => void): void;
     getTenants(): Promise<string[]>;
     generateDeviceCreateEventForActiveDevices(): void;
   }
-  
-  export class UnknownDeviceError extends Error {}
-  export class UnknownTenantError extends Error {}
-  export class InitializationError extends Error {}
+
+  export class UnknownDeviceError extends Error { }
+  export class UnknownTenantError extends Error { }
+  export class InitializationError extends Error { }
 }
